0fe5b7147cb4c5dddca1b94376e6f6ea400969a4
bhoon <leafloveu@naver.com>
2025-06-02 16:01:17 +0900

    add: Multiplier 구현

--- src/main/java/calculator/MultiplierImpl.java
+++ src/main/java/calculator/MultiplierImpl.java
@@ -2,8 +2,35 @@ package calculator;
 
 public class MultiplierImpl implements Multiplier {
 
+    private final Flipper flipper;
+    private final Adder adder;
+
+    public MultiplierImpl(Flipper flipper, Adder adder) {
+        this.flipper = flipper;
+        this.adder = adder;
+    }
+
     @Override
     public int multiply(int a, int b) {
-        return 0;
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+
+        boolean isNegative = false;
+        if(a < 0) {
+            isNegative = true;
+            a = flipper.flip(a);
+        }
+        if(b < 0) {
+            isNegative = !isNegative;
+            b = flipper.flip(b);
+        }
+
+        int result = 0;
+        for (int i = 0; i < b; i++) {
+            result = adder.add(result, a);
+        }
+
+        return isNegative ? flipper.flip(result) : result;
     }
 }
