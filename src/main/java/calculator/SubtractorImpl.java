f582c51d219c93e10865dcc08305d722f986a33f
bhoon <leafloveu@naver.com>
2025-06-02 16:18:37 +0900

    add: Subtractor 구현

--- src/main/java/calculator/SubtractorImpl.java
+++ src/main/java/calculator/SubtractorImpl.java
@@ -2,8 +2,18 @@ package calculator;
 
 public class SubtractorImpl implements Subtractor {
 
+    private final Flipper flipper;
+    private final Adder adder;
+
+    public SubtractorImpl(Flipper flipper, Adder adder) {
+        this.flipper = flipper;
+        this.adder = adder;
+    }
+
     @Override
     public int subtract(int a, int b) {
-        return 0;
+        b = flipper.flip(b);
+
+        return adder.add(a,b);
     }
 }
